de87cf4877f98571e038b9b5858b657154035bc1
Cristina Secu <secu.cristina@yahoo.com>
2024-05-09 21:17:33 -0400

    refactr code and query searched country from data retrieved from initial api request

--- script.js
+++ script.js
@@ -4,78 +4,65 @@ const searchBtn = document.querySelector('.search__btn');
 const searchCont = document.querySelector('.search__container');
 const goBackBtn = document.querySelector('.goBack__btn');
 
-//1. Make API request and display all countries
+//This will store all countries data
+let allCountries = [];
 
-const callAPI = async function (url, func) {
+//1. Make API request and fetch all countries
+const fetchCountries = async function (url, func) {
   try {
     const request = await fetch(url);
-    const response = await request.json();
-    func(response);
+    allCountries = await request.json();
+    displayCountry(allCountries);
+    console.log('all countries', allCountries);
   } catch (err) {
     console.error(err);
   }
 };
 
-const displayCountry = function (data) {
-  console.log('data is ', data);
-  data.forEach(country => {
-    const countryCard = `<div class="country">
-    <img class="country__img" src="${country.flags.png}" alt="${country.flags.alt}"/>
-    <div class="country__data">
-      <h3 class="country__name">${country.name.official}</h3>
-      <p class="country__population">${country.population}</p>
-      <p class="country__region">${country.region}</p>
-      <p class="country__capital">${country.capital}</p>
-    </div>
-  </div>`;
+const createCountryCard = function (country) {
+  return `<div class="country">
+  <img class="country__img" src="${country.flags.png}" alt="${country.flags.alt}"/>
+  <div class="country__data">
+    <h3 class="country__name">${country.name.official}</h3>
+    <p class="country__population">${country.population}</p>
+    <p class="country__region">${country.region}</p>
+    <p class="country__capital">${country.capital}</p>
+  </div>
+</div>`;
+};
 
-    container.insertAdjacentHTML('beforeEnd', countryCard);
+//Display all countries or a singler country (per user's search)
+const displayCountry = function (data) {
+  container.innerHTML = '';
+  data.forEach(country => {
+    container.insertAdjacentHTML('beforeEnd', createCountryCard(country));
   });
 };
 
-//When page loads, display all countries
-callAPI('https://restcountries.com/v3.1/all', displayCountry);
-
-//Function that displays the country that the user searched for
-const displaySearchResult = function (data) {
-  const country = data[0];
-  const countryCard = `<div class="country">
-      <img class="country__img" src="${country.flags.png}" alt="${country.flags.alt}"/>
-      <div class="country__data">
-        <h3 class="country__name">${country.name.official}</h3>
-        <p class="country__population">${country.population}</p>
-        <p class="country__region">${country.region}</p>
-        <p class="country__capital">${country.capital}</p>
-      </div>
-    </div>`;
-  container.innerHTML = '';
-  container.insertAdjacentHTML('beforeEnd', countryCard);
-  searchCont.style.display = 'none';
-  goBackBtn.classList.remove('btn__hide');
-  goBackBtn.classList.add('.btn__show');
-};
-
+//Event Listeners
 searchBtn.addEventListener('click', function () {
-  let searchValue = searchBar.value;
+  let searchValue = searchBar.value.toLowerCase();
   searchBar.value = '';
   searchCountry(searchValue);
+
+  searchCont.style.display = 'none';
+  goBackBtn.classList.remove('btn__hide');
+  goBackBtn.classList.add('btn__show');
 });
 
 goBackBtn.addEventListener('click', function () {
-  container.innerHTML = '';
-  callAPI('https://restcountries.com/v3.1/all', displayCountry);
+  displayCountry(allCountries);
   goBackBtn.classList.remove('btn__show');
   goBackBtn.classList.add('btn__hide');
   searchCont.style.display = 'block';
-
-  /* NOTE FOR LATER:
-  Think of a way to avoid making a second API request to display all countries
-  when Go Back btn is clicked*/
 });
 
-const searchCountry = function (country) {
-  const getCountry = callAPI(
-    `https://restcountries.com/v3.1/name/${country}`,
-    displaySearchResult
+const searchCountry = function (searchedValue) {
+  const filteredCountries = allCountries.filter(country =>
+    country.name.common.toLowerCase().includes(searchedValue)
   );
+  displayCountry(filteredCountries);
 };
+
+//Initial call to fetch countries
+fetchCountries('https://restcountries.com/v3.1/all');
